f4d6f73903aa49bfc029205f5d0b4ae0aaebff41
ak821109 <kapilgaire2096@gmail.com>
2024-12-24 18:49:21 +0530

    update value

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     system("cls");
+    int num;
     cout << "\n\n\n\n\n\t\t\t\t\t ***** CALCULATOR *****";
     cout << "\n\n\t\t";
     cout << "\n\n\t\t 1. ADDITION.";
@@ -11,5 +12,18 @@ int main()
     cout << "\n\n\t\t 4. DIVIDING.";
     cout << "\n\n\t\t 5. PERCENTAGE.";
     cout<<"\n\n\n\t\t\t";
+    cout<<"choose the above option:";
+    cin>>num;
+    switch(num)
+    {
+        case 1:
+        add();
+    }
     return 0;
+}
+int add()
+{
+    double num1,num2;
+    cout<<"Enter the first and second number"; 
+
 }
