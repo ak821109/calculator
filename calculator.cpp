c93f613df9ea503fa3cc3a46489e39ecc018ece0
ak821109 <kapilgaire2096@gmail.com>
2024-12-24 15:15:23 +0530

    add new code

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    cout << "\n\n\n\n\n\t\t\t\t\t ***** CALCULATOR *****";
+    cout << "\n\n\t\t";
+    cout << "\n\n\t\t 1. ADDITION.";
+    cout << "\n\n\t\t 2. SUBTRACT.";
+    cout << "\n\n\t\t 3. MULTIPLICATION.";
+    cout << "\n\n\t\t 3. DIVIDING.";
+    cout << "\n\n\t\t 3. PERCENTAGE.";
+    return 0;
+}
